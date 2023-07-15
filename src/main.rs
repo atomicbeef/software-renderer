8bde71125753e7d99c084059988c0d6365f6ab40
Theodore Gregor <teddyg522@gmail.com>
2023-07-15 15:19:11 +0300

    Fix unintentiontal rotation resetting

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ fn update(
     triangles_to_render.clear();
 
     // Animate mesh
-    mesh.rotation = if settings.rotate { mesh.rotation + settings.rotation } else { Vec3::default() };
+    mesh.rotation = if settings.rotate { mesh.rotation + settings.rotation } else { mesh.rotation };
     mesh.translation.x = if settings.translate { 2.0 * elapsed_time.sin() } else { 0.0 };
     mesh.translation.y = if settings.translate { 2.0 * elapsed_time.cos() } else { 0.0 };
     mesh.translation.z = 5.0;
